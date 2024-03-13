187de1635360b66eb2dd0b801f89f1047f5d008b
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-03-13 10:20:58 +0300

    analysis of the problem with solution #28

---
+++ easy/28.find_the_index_of_the_first_occurrence_in_a_string.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+
+def str_str(haystack: str, needle: str) -> int:
+    result = haystack.find(needle)
+
+    return print(result)
+
+
+haystack = "sadbutsad"
+needle = "sad"
+
+str_str(haystack, needle)
